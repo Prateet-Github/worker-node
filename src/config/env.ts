dc9a0e95858a4260ffe2835cd21e656e9752254f
Prateet Tiwari <prateettiwari29@gmail.com>
2026-03-24 21:55:05 +0530

    feat: add AWS S3 client configuration and update environment variables

--- src/config/env.ts
+++ src/config/env.ts
@@ -3,8 +3,11 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const env = {
-  REDIS_HOST: process.env.REDIS_HOST || "127.0.0.1",
-  REDIS_PORT: Number(process.env.REDIS_PORT) || 6379,
-  AWS_REGION: process.env.AWS_REGION || "",
-  S3_BUCKET: process.env.S3_BUCKET || "",
+  REDIS_HOST: process.env.REDIS_HOST!,
+  REDIS_PORT: parseInt(process.env.REDIS_PORT!, 10),
+  AWS_REGION: process.env.AWS_REGION!,
+  AWS_ACCESS_KEY: process.env.AWS_ACCESS_KEY!,
+  AWS_SECRET_KEY: process.env.AWS_SECRET_KEY!,
+  S3_RAW_BUCKET: process.env.S3_RAW_BUCKET!,
+  S3_PROD_BUCKET: process.env.S3_PROD_BUCKET!,
 };
